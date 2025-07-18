12b6d678bad7c07dfde38bdcd05f6fc4f3846f2a
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-07-17 18:30:50 -0600

    feat: add EmployeeDto class with validation for employee attributes

---
+++ src/main/java/mx/edu/utez/vehicleManager/module/employee/EmployeeDto.java
@@ -0,0 +1,54 @@
+package mx.edu.utez.vehicleManager.module.employee;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class EmployeeDto {
+    @Pattern(regexp = "^[^<>]*$", message = "No se permiten los caracteres < o >")
+    @Size(max = 50, message = "El nombre no puede tener más de 50 caracteres")
+    private String full_name;
+
+    @Pattern(regexp = "^[^<>]*$", message = "No se permiten los caracteres < o >")
+    @Size(max = 10, message = "El teléfono no puede tener más de 10 caracteres")
+    private String phone;
+
+    @Pattern(regexp = "^[^<>]*$", message = "No se permiten los caracteres < o >")
+    @Email(message = "Este correo electrónico no es válido")
+    private String email;
+
+    private Long roleId;
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name != null ? full_name.trim() : null;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone != null ? phone.trim() : null;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email != null ? email.trim() : null;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+}
