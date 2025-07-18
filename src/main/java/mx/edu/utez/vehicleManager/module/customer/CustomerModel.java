a2294f66f6cb49787d9edccc082c1f4385d1a16c
Leo-Noriega <leonoriega100@gmail.com>
2025-07-18 12:20:15 -0600

    feat(customer): update CustomerModel with new changes

--- src/main/java/mx/edu/utez/vehicleManager/module/customer/CustomerModel.java
+++ src/main/java/mx/edu/utez/vehicleManager/module/customer/CustomerModel.java
@@ -19,60 +19,60 @@ import mx.edu.utez.vehicleManager.module.vehicle.VehicleModel;
 @Table(name = "customer")
 public class CustomerModel {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String full_name;
-    private String phone;
-    private String email;
+	private String full_name;
+	private String phone;
+	private String email;
 
-    @OneToMany(mappedBy = "customer")
-    @JsonIgnore
-    private List<VehicleModel> vehicles;
+	@OneToMany(mappedBy = "customer")
+	@JsonIgnore
+	private List<VehicleModel> vehicles;
 
-    @ManyToOne
-    @JoinColumn(name = "employee_id")
-    private EmployeeModel employee;
+	@ManyToOne
+	@JoinColumn(name = "employee_id")
+	private EmployeeModel employee;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getFull_name() {
-        return full_name;
-    }
+	public String getFull_name() {
+		return full_name;
+	}
 
-    public void setFull_name(String full_name) {
-        this.full_name = full_name;
-    }
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
 
-    public String getPhone() {
-        return phone;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public List<VehicleModel> getVehicles() {
-        return vehicles;
-    }
+	public List<VehicleModel> getVehicles() {
+		return vehicles;
+	}
 
-    public void setVehicles(List<VehicleModel> vehicles) {
-        this.vehicles = vehicles;
-    }
+	public void setVehicles(List<VehicleModel> vehicles) {
+		this.vehicles = vehicles;
+	}
 
 }
