2a72c218a029f38417ff284fb517f397f9bcb4fc
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-07-30 12:55:41 -0600

    feat: implement SaleController and SaleService for sale generation functionality

---
+++ src/main/java/mx/edu/utez/vehicleManager/module/sale/SaleController.java
@@ -0,0 +1,25 @@
+package mx.edu.utez.vehicleManager.module.sale;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/sale")
+public class SaleController {
+
+    private final SaleService saleService;
+
+    public SaleController(SaleService saleService) {
+        this.saleService = saleService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Object> generateSale(@RequestBody @Valid SaleDto dto) {
+        return saleService.generateSale(dto);
+    }
+}
