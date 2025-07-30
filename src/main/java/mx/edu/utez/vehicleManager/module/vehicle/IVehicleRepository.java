a0c69720e4f3048140f071b3fd6b3d24c5614a1e
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-07-30 13:16:25 -0600

    feat: add queries to count sold and available vehicles in IVehicleRepository

--- src/main/java/mx/edu/utez/vehicleManager/module/vehicle/IVehicleRepository.java
+++ src/main/java/mx/edu/utez/vehicleManager/module/vehicle/IVehicleRepository.java
@@ -1,9 +1,15 @@
 package mx.edu.utez.vehicleManager.module.vehicle;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IVehicleRepository extends JpaRepository<VehicleModel, Long> {
 
+    @Query("SELECT COUNT(v) FROM VehicleModel v WHERE v.sale_date IS NOT NULL")
+    long getSoldVehiclesCount();
+
+    @Query("SELECT COUNT(v) FROM VehicleModel v WHERE v.sale_date IS NULL")
+    long getAvailableVehiclesCount();
 }
