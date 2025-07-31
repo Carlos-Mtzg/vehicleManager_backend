cdade875d1cf668e32052fd5b29a2c4d56a45824
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-07-31 12:34:05 -0600

    feat: add getter method for enabled field in UserModel

--- src/main/java/mx/edu/utez/vehicleManager/module/user/UserModel.java
+++ src/main/java/mx/edu/utez/vehicleManager/module/user/UserModel.java
@@ -113,6 +113,10 @@ public class UserModel implements UserDetails {
         this.employee = employee;
     }
 
+    public boolean getEnabled() {
+        return enabled;
+    }
+
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
