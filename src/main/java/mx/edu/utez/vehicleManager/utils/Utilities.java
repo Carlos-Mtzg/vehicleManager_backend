7b9562ec57f3a49cfa879c64ce3ff3c4635e8658
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-07-30 21:41:42 -0600

    feat: add Utilities class for response generation and error handling

--- src/main/java/mx/edu/utez/vehicleManager/utils/Utilities.java
+++ src/main/java/mx/edu/utez/vehicleManager/utils/Utilities.java
@@ -1,4 +1,4 @@
-package mx.edu.utez.vehicleManager.config;
+package mx.edu.utez.vehicleManager.utils;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -26,6 +26,24 @@ public class Utilities {
             map.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
             map.put("message", e.getMessage());
             map.put("data", null);
+            return new ResponseEntity<>(map, status);
+        }
+    }
+
+    public static ResponseEntity<Object> authResponse(HttpStatus status, String message, Object data) {
+        Map<String, Object> map = new HashMap<>();
+        try {
+            map.put("date", new Date());
+            map.put("status", status.value());
+            map.put("message", message);
+            map.put("token", data);
+            return new ResponseEntity<>(map, status);
+        } catch (Exception e) {
+            map.clear();
+            map.put("date", new Date());
+            map.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+            map.put("message", e.getMessage());
+            map.put("token", null);
             return new ResponseEntity<>(map, status);
         }
     }
