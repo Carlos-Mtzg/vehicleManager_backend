89f0368be005ba9368f1e0d1eee03237db5ca389
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-08-04 12:25:27 -0600

    feat: add Swagger configuration for API documentation

---
+++ src/main/java/mx/edu/utez/vehicleManager/config/SwaggerConfig.java
@@ -0,0 +1,8 @@
+package mx.edu.utez.vehicleManager.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Vehicle Manager API", description = "API RESTful para la gestión integral de vehículos, usuarios, empleados, clientes, ventas y servicios. Proporciona endpoints seguros para la administración de inventario, control de usuarios y operaciones de negocio en una agencia automotriz. Incluye autenticación JWT, control de roles y protección avanzada de datos.", version = "1.0.0"))
+public class SwaggerConfig {
+}
